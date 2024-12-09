7773391f18346a85f2821116929653b6feb35560
EndrXOriginial <chris.pin@hotmail.ca>
2024-12-09 13:30:49 -0500

    Started the README file

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
     return (
         <nav className="nav">
             <div className="wrapper nav-content">
-                <img src="" alt="Logo from the game Deadlock" />
+                <a href="*" className="logo"><img src="" alt="Logo from the game Deadlock" /></a>
                 <ul className="nav-content-ul">
                     <li className="nav-content-li"><a href="*">Home</a></li>
                     <li className="nav-content-li"><a href="*">Items</a></li>
