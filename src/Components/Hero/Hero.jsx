d27d55b6566ea6e472e835683db26c67e366ac64
EndrXOriginial <chris.pin@hotmail.ca>
2024-12-10 13:22:26 -0500

    Added new branch with template css for the hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,8 +5,8 @@ export const Hero = () => {
         <div className="hero">
             <div className="hero-media"><img src="" alt="Backgroun image of the hero" className="hero-media-img" /></div>
             <div className="hero-content">
-                <h1 className="hero-content-title">Title</h1>
-                <p className="hero-content-subtitle">The subtitle of the page</p>
+                <h1 className="hero-content-title">Pickled</h1>
+                <p className="hero-content-subtitle">Il aime quand on y mange le pickle</p>
             </div>
         </div>
     );
