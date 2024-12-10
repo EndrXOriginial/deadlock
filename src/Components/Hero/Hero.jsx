9eadde40ce886e9a4a00d7e30a599d530535904b
EndrXOriginial <chris.pin@hotmail.ca>
2024-12-10 13:42:18 -0500

    ajusted css for the hero's image

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,10 @@
 import './hero.css';
+import corni from '../../assets/images/corni.jpg';
 
 export const Hero = () => {
     return (
         <div className="hero">
-            <div className="hero-media"><img src="" alt="Backgroun image of the hero" className="hero-media-img" /></div>
+            <div className="hero-media"><img src={corni} alt="background of the hero" className="hero-media-img" /></div>
             <div className="hero-content">
                 <h1 className="hero-content-title">Pickled</h1>
                 <p className="hero-content-subtitle">Il aime quand on y mange le pickle</p>
