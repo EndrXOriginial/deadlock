c7b8acfabc1134002dc41172e7a1869ac7280a9b
EndrXOriginial <chris.pin@hotmail.ca>
2024-12-12 13:10:00 -0500

    added all images and preloaded them. Check comment in imageFetcher

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,11 @@
 import './hero.css';
 import corni from '../../assets/images/corni.jpg';
+const { splashArt, mapIcon } = require('../../assets/images/imageFetcher');
 
 export const Hero = () => {
     return (
         <div className="hero">
-            <div className="hero-media"><img src={corni} alt="background of the hero" className="hero-media-img" /></div>
+            <div className="hero-media"><img src={splashArt['dynamo']} alt="background of the hero" className="hero-media-img" /></div>
             <div className="hero-content">
                 <h1 className="hero-content-title">Pickled</h1>
                 <p className="hero-content-subtitle">Il aime quand on y mange le pickle</p>
