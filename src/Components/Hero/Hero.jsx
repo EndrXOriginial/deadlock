a1128bce6b55b5f0ef7984f02433597dbd728b84
EndrXOriginial <chris.pin@hotmail.ca>
2024-12-12 14:51:31 -0500

    added comments in the Hero to explain how to use it

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,20 +1,22 @@
 import './hero.css';
 import corni from '../../assets/images/corni.jpg';
-const { splashArt, gallery, icons } = require('../../assets/images/imageFetcher');
+const {icons} = require('./../../assets/images/imageFetcher');
 
-export const Hero = ({content}) => {
-    console.log(gallery['midSunset'])
+// The parent component should give the Hero component a 'title' props, a 'subtitle' props.
+// For the bg image, simply put the desired image.
+// If you want the logo use 'fullLogo' or 'logo' in the title.
+export const Hero = ({title, subtitle, bgImage}) => {
     return (
         <div className="hero">
             <div className="hero-media">
                 <div className="hero-media-filter"></div>
-                <img src={gallery['midSunset']} alt="background of the hero" className="hero-media-img" />
+                <img src={bgImage} alt="background of the hero" className="hero-media-img" />
             </div>
             <div className="hero-content">
                 {
-                    content.title === 'fullLogo' || content.title === 'logo' ? <img src={icons[content.title]} alt='Logo of the game' /> : <h1 className="hero-content-title">{content.title}</h1>
+                    title === 'fullLogo' || title === 'logo' ? <img src={icons[title]} alt='Logo of the game' /> : <h1 className="hero-content-title">{title}</h1>
                 }
-                <p className="hero-content-subtitle">{content.subtitle}</p>
+                <p className="hero-content-subtitle">{subtitle}</p>
             </div>
         </div>
     );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,11 @@
 import { Hero } from "../Components/Hero/Hero";
-const { icons } = require('./../assets/images/imageFetcher');
+const { icons, gallery } = require('./../assets/images/imageFetcher');
 
 //Page for the home page of the site.
 export const Home = () => {
     return (
         <div>
-            <Hero content={{title: 'fullLogo', subtitle: 'Invite a friend to join Deadlock'}} />
+            <Hero title={'fullLogo'} subtitle={'Invite a friend to join Deadlock'} bgImage={gallery['midSunset']} />
         </div>
  
     )
