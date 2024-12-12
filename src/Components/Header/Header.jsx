f5c16bb6a1a2068bef08a4fb889d001ba9a06e88
EndrXOriginial <chris.pin@hotmail.ca>
2024-12-12 15:40:29 -0500

    Added logo svg to header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router';
 import './header.css';
+import logo from './../../assets/icons/logo.svg';
 
 // Menu component with manual inputs. Will upgrade to a dynamic menu from the App.js later.
 export const Header = () => {
     return (
         <nav className="nav">
             <div className="wrapper nav-content">
-                <a href="*" className="logo"><img src="" alt="Logo from the game Deadlock" /></a>
+                <a href="*" className="logo"><img src={logo} alt="Logo from the game Deadlock" /></a>
                 <ul className="nav-content-ul">
                     <li className="nav-content-li"><Link to="/">Home</Link></li>
                     <li className="nav-content-li"><Link to="/items">Items</Link></li>
