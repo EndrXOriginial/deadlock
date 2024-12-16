11dfe82b29d4db72c70b95f306a95b0c7b2d1924
EndrXOriginial <chris.pin@hotmail.ca>
2024-12-16 11:53:31 -0500

    cleaned the champCard file

--- src/Components/ChampList/ChampCard/ChampCard.jsx
+++ src/Components/ChampList/ChampCard/ChampCard.jsx
@@ -1,21 +1,11 @@
 import { Link } from 'react-router';
-import { cards, gallery, inGameModel } from '../../../assets/images/imageFetcher';
+import { gallery, inGameModel } from '../../../assets/images/imageFetcher';
 import './champCard.css';
 
 export const ChampCard = ({champ}) => {
     const {name, id} = champ;
-    const regex = /&/ ;
 
     return (
-        // <Link to={`${id}`} className='champCard-link'>
-        // <div className="champCard">
-        //     <div className="champCard-media"><img src={cards[`${name} Mobile`]} alt={`screenshot of the main menu of the game`} className="champCard-media-img" /></div>
-        //     <div className="champCard-content">
-        //         <h3 className="champCard-content-name">{name}</h3>
-        //     </div>
-        // </div>
-        // </Link>
-
         <Link to={`${id}`} className='champCard-link'>
         <div className="champCard">
             <div className="linear-filter"></div>
