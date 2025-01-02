087631de9bcd68efc1c47250136eeac0be0ca9b5
EndrXOriginial <chris.pin@hotmail.ca>
2025-01-02 12:53:52 -0500

    added an imbue tag and... yeah

--- src/Components/ItemsList/ItemCard/ItemCard.jsx
+++ src/Components/ItemsList/ItemCard/ItemCard.jsx
@@ -1,6 +1,8 @@
 import './itemCard.css';
 
 export const ItemCard = ({item, handleClick}) => {
+    const imbueItem = ['Quicksilver Reload', 'Improved Reach', 'Superior Cooldown', 'Superior Duration', 'Surge of Power', 'Mystic Reverb']
+    
     return (
         <div className="item" onClick={handleClick} id={item.id}>
             <div className={item.is_active_item ? `item-media ${item.item_slot_type}Active` : `item-media ${item.item_slot_type}`}><img src={item.image} alt={`Logo of the ${item.name} item`} /></div>
@@ -9,6 +11,9 @@ export const ItemCard = ({item, handleClick}) => {
             </div>
             <div className={item.is_active_item ? "active-banner" : "none"}>
                 <p className='p-xs'>Active</p>
+            </div>
+            <div className={imbueItem.includes(item.name) ? "active-banner" : "none"}>
+                <p className='p-xs'>Imbue</p>
             </div>
         </div>
     )
--- src/Components/ItemsList/ItemInfo/ItemInfo.jsx
+++ src/Components/ItemsList/ItemInfo/ItemInfo.jsx
@@ -2,19 +2,21 @@ import { icons } from '../../../assets/images/imageFetcher';
 import './itemInfo.css';
 
 export const ItemInfo = ({selectedItem, closeItemModal}) => {
+    // console.log(Object.keys(selectedItem.description).includes('passive') || (Object.keys(selectedItem.description).includes('desc') && Object.keys(selectedItem.description).includes('active')));
+    console.log(selectedItem);
 
     return (
         <div className="item-info">
-            <div className="item-info-media">
+            <div className={`item-info-media ${selectedItem.item_slot_type}`}>
                 <img src={selectedItem.image} alt="icon of the item" className='item-info-icon'/>
-                <div className="item-info-media-text">
+                <div className='item-info-media-text'>
                     <h2>{selectedItem.name}</h2>
                     <div className="cost-div"><img src={icons['souls']} alt="icon of the souls in Deadlock" /><p className='souls-cost'>{selectedItem.cost}</p></div>
                 </div>
             </div>
             <div className="item-info-buff">
-                {/* {console.log(selectedItem)} */}
-                <div className='item-info-state-container'><p className="p-xs">Active</p><div className="active-cooldown"><img src={icons['cooldown']} alt="cooldown icon" />{selectedItem.properties.AbilityCooldown.value}s</div></div>
+                {   Object.keys(selectedItem.description).includes('passive') || (Object.keys(selectedItem.description).includes('desc') && !Object.keys(selectedItem.description).includes('active')) ? <div className='item-info-state-container'><p className="p-xs">Passive</p>{ selectedItem.properties.AbilityCooldown.value != 0 && !selectedItem.is_active_item ? <div className="active-cooldown"><img src={icons['cooldown']} alt="cooldown icon" />{selectedItem.properties.AbilityCooldown.value}s</div> : <></> }</div> : <></>  }
+                {  selectedItem.is_active_item ? <div className='item-info-state-container'><p className="p-xs">Active</p><div className="active-cooldown"><img src={icons['cooldown']} alt="cooldown icon" />{selectedItem.properties.AbilityCooldown.value}s</div></div> : <></>  }
             </div>
             <div onClick={closeItemModal} className="item-info-x">
                 <img src={icons['x']} alt="X to close the modal" className="icon-x"/>
