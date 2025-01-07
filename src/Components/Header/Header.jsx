32b40adf196f91c385adfa3c01fbc5a4542ecddf
EndrXOriginial <chris.pin@hotmail.ca>
2025-01-07 10:19:59 -0500

    added animation for the nav menu

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -40,7 +40,6 @@ export const Header = () => {
                     let element = navContent.childNodes[i];
                     if (element.childNodes.length != 0) {
                         for (let j = 0; j < element.childNodes.length; j++) {
-                            console.log(element.childNodes[j]);
                             element.childNodes[j].classList.remove('is-active');
                         }
                     } else {
@@ -71,16 +70,16 @@ export const Header = () => {
             <Link to={'/'} className="logo"><img src={logo} alt="Logo from the game Deadlock" /></Link>
             <img src={icons['x']} alt="X button" className='nav-x' onClick={toggleMenu}/>
             <div className="nav-content">
-                <ul className="nav-content-ul">
-                    <li className="nav-content-li" data-scrolly='from-left'><Link to="/"><h2>Homepage</h2></Link></li>
-                    <li className="nav-content-li" data-scrolly='from-left'><Link to="/heroes"><h2>Heroes</h2></Link></li>
-                    <li className="nav-content-li" data-scrolly='from-left'><Link to="/items"><h2>Items</h2></Link></li>
-                    <li className="nav-content-li" data-scrolly='from-left'><Link to="/guide"><h2>Guide</h2></Link></li>
+                <ul className="nav-content-ul" style={{'--length': 4}}>
+                    <li className="nav-content-li" style={{'--delay': 3}} data-scrolly='from-left'><Link to="/"><h2>Homepage</h2></Link></li>
+                    <li className="nav-content-li" style={{'--delay': 2}} data-scrolly='from-left'><Link to="/heroes"><h2>Heroes</h2></Link></li>
+                    <li className="nav-content-li" style={{'--delay': 1}} data-scrolly='from-left'><Link to="/items"><h2>Items</h2></Link></li>
+                    <li className="nav-content-li" style={{'--delay': 0}} data-scrolly='from-left'><Link to="/guide"><h2>Guide</h2></Link></li>
                 </ul>
-                <hr className="separator" data-scrolly='from-left'/>
+                <hr className="separator" style={{'--delay': 4}} data-scrolly='from-left'/>
                 <div className="nav-content-policy">
-                    <Link to={'/privacy-policy'} data-scrolly='from-left'><p className='p-s'>Privacy Policy</p></Link>
-                    <Link to={'/terms-of-services'} data-scrolly='from-left'><p className='p-s'>Terms of Services</p></Link>
+                    <Link to={'/privacy-policy'} style={{'--delay': 5}} data-scrolly='from-left'><p className='p-s'>Privacy Policy</p></Link>
+                    <Link to={'/terms-of-services'} style={{'--delay': 6}} data-scrolly='from-left'><p className='p-s'>Terms of Services</p></Link>
                 </div>
             </div>
             </div>
